0cd84c76eb6f217a9159d7fea15769a86810299f
Roydelangel <roydelangelaguero@gmail.com>
2026-06-08 04:05:53 -0400

    components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import AuthLayout from './layout/AuthLayout';
 import Login from './pages/Login';
+import Registrar from './pages/Registrar';
+import OlvidePassword from './pages/OlvidePassword';
+import ConfirmarCuenta from './pages/ConfirmarCuenta';
 
 function App() {
   
@@ -8,11 +11,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AuthLayout />} />
+        <Route path='/' element={<AuthLayout />}>
           <Route index element={<Login />} />
+          <Route path='registrar' element={<Registrar />} />
+          <Route path='olvide-password' element={<OlvidePassword />} />
+          <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
-}
+};
 
 export default App;
