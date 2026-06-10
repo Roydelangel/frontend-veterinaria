43eae28fadfdfd7bab61f374fb0044bc0df68cfb
Roydelangel <roydelangelaguero@gmail.com>
2026-06-09 22:48:44 -0400

    validacion form

--- src/pages/OlvidePassword.jsx
+++ src/pages/OlvidePassword.jsx
@@ -1,8 +1,7 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const OlvidePassword = () => {
-
-  
 
   return (
     <>
--- src/pages/Registrar.jsx
+++ src/pages/Registrar.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Registrar = () => {
+
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repetirPassword, setRepetirPassword] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if ([nombre, email, password, repetirPassword].includes('')) {
+
+
+      return;
+    };
+
+    if (password !== repetirPassword) {
+      
+
+      return;
+    };
+
+    if (password < 6) {
+      
+    };
+    
+  };
+
   return (
     <>
       <div>
@@ -10,33 +38,38 @@ const Registrar = () => {
       </div>
 
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="my-5">
               <label className="uppercase text-gray-600 block text-xl font-bold">
                 Nombre
               </label>
-              <input type="text" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Tu nombre"/>
+              <input type="text" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Tu nombre" 
+              value={nombre} onChange={e => setNombre(e.target.value)}/>
             </div>
             
             <div className="my-5">
               <label className="uppercase text-gray-600 block text-xl font-bold">
                 Email
               </label>
-              <input type="email" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Email de registro"/>
+              <input type="email" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Email de registro" 
+              value={email} onChange={e => setEmail(e.target.value)}/>
+              
             </div>
             
             <div className="my-5">
               <label className="uppercase text-gray-600 block text-xl font-bold">
                 Password
               </label>
-              <input type="password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Tu password"/>
+              <input type="password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Tu password"
+              value={password} onChange={e => setPassword(e.target.value)}/>
             </div>
             
             <div className="my-5">
               <label className="uppercase text-gray-600 block text-xl font-bold">
                 Repetir Password
               </label>
-              <input type="password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Repite tu password"/>
+              <input type="password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Repite tu password"
+              value={repetirPassword} onChange={e => setRepetirPassword(e.target.value)}/>
             </div>
 
             <input type="submit" value="Crear cuenta" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 
