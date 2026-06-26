df4f011c18f8c6be59ebdeb1b294e8a193547a7a
Roydelangel <roydelangelaguero@gmail.com>
2026-06-26 00:01:24 -0400

    peticion post auth

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -2,13 +2,17 @@ import { useState, useEffect, createContext } from 'react';
 
 const AuthContext = createContext();
 
-
-
 const AuthProvider = ({ children }) => {
     
+    const [auth, setAuth] = useState({});
 
     return(
-        <AuthContext.Provider>
+        <AuthContext.Provider
+            value={{
+                auth,
+                setAuth
+            }}
+        >
             {children}
 
         </AuthContext.Provider>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,44 @@
-import { Link } from "react-router-dom";
-import PosterDeco from "../components/PosterDeco";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import PosterDeco from '../components/PosterDeco';
+import useAuth from '../hooks/useAuth';
+import Alerta from '../components/Alerta';
+import clienteAxios from '../../config/axios';
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    if ( [email, password].includes('') ) {
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      });
+
+      return;
+    };
+
+    try {
+      const { data } = await clienteAxios.post('/veterinarios/login', { email, password });
+
+      localStorage.setItem('token', data.token)
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      });
+    };
+
+
+  };
+
+  const { msg } = alerta;
+  
   return (
     <>
       <PosterDeco />
@@ -14,19 +51,26 @@ const Login = () => {
         </div>
 
         <div className="card-hover shadow-lg px-8 py-10 rounded-xl bg-white">
-          <form>
+
+          { msg && <Alerta
+            alerta={alerta}
+          />}
+
+          <form onSubmit={handleSubmit}>
             <div className="my-5">
               <label className="uppercase text-gray-600 block text-xl font-bold">
                 Email
               </label>
-              <input type="email" className="input-focus border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Email de registro"/>
+              <input type="email" className="input-focus border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Email de registro"
+                value={email} onChange={e => setEmail(e.target.value)}/>
             </div>
 
             <div className="my-5">
               <label className="uppercase text-gray-600 block text-xl font-bold">
                 Password
               </label>
-              <input type="password" className="input-focus border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Password"/>
+              <input type="password" className="input-focus border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Password"
+                value={password} onChange={e => setPassword(e.target.value)}/>
             </div>
 
             <input type="submit" value="Iniciar Sesion" className="btn-hover bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"/>
